f36adeab3e269577aa67f2ba5fad56537ff4191b
karabanick <nikmunuhe@gmai.com>
2023-09-10 09:03:56 +0000

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program that finds the sum of positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: result
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	char *ptr;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		ptr = argv[i];
+		for (j = 0; ptr[j] != '\0'; j++)
+		{
+			if (ptr[j] < '0' || ptr[j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(ptr);
+	}
+	printf("%d", sum);
+
+	return (0);
+}
