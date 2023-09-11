ab7d1089c0bc111a43de5361874f6655df10b33e
karabanick <nikmunuhe@gmai.com>
2023-09-11 10:38:48 +0000

    object like macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO
+#define OBJECT_LIKE_MACRO
+
+/**
+ * OBJECT_LIKE_MACRO - header file that defines a macro
+ * SIZE - macro name
+ * 1024 - abbreviation
+ */
+
+#define SIZE 1024
+
+#endif
