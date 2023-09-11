158193734ea775b7dd7129418aeaa02a1d9bb6cf
karabanick <nikmunuhe@gmai.com>
2023-09-11 11:00:12 +0000

    __FILE__ predefined macro

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the name of the file it was compiled on
+ * using predefined macro __file__
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
