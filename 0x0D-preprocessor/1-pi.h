12acf2d024cbe1e78488df3272e5b0f9a056c8aa
karabanick <nikmunuhe@gmai.com>
2023-09-11 10:46:13 +0000

    PI macro

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef _PI
+#define _PI
+
+/**
+ * _P1 - header file that defines a macro named PI
+ * PI - name of macro
+ * 3.14159265359 - abbreviation
+ */
+
+#define PI 3.14159265359
+
+#endif
