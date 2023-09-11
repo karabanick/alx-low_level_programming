32c177aaa3b4e9a6a3182a5eb37ac088ecde8440
karabanick <nikmunuhe@gmai.com>
2023-09-11 14:47:50 +0000

    dog struct header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure
+ * @name: element
+ * @age: element
+ * @owner: element
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
