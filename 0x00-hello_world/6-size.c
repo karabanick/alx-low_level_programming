7c75ed731475527359fce120d6922d6c74975f20
karabanick <nikmunuhe@gmail.com>
2023-12-15 00:01:19 +0300

    size of datatypes on a 64bit machine

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - C program to print sizeof datatypes on a system
+ * Return: 0 (Successful)
+ */
+
+int main(void)
+{
+	char i;
+	int j;
+	long int k;
+	long long int l;
+	float m;
+
+	printf("Size of a char: %lu byte(s)\n",(unsigned long)sizeof(i));
+	printf("Size of a int: %lu byte(s)\n",(unsigned long)sizeof(j));
+	printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(k));
+	printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(l));
+	printf("Size of a float: %lu byte(s)\n",(unsigned long)sizeof(m));
+
+	return (0);
+}	
