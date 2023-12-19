a28b785aa6f654a1022e8cba80d62c7c5bd1bed4
karabanick <nikmunuhe@gmail.com>
2023-12-19 19:05:47 +0300

    combinations of single-digit numbers
    esc

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,20 +3,24 @@
 /**
  * main - Program that prints possible combinations of single-digit numbers
  * separated by a comma and a space
- * Return: 0 (Successful)
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
 	int g;
 
-	g = 0;
 	for (g = 0; g <= 9; g++)
 	{
 		putchar(g + '0');
-		putchar(',');
-		putchar(' ');
+
+		if (g < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar ('\n');
+
 	return (0);
 }
