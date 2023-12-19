a71126a90fc941b932aa661abd46f6cd52c2e673
karabanick <nikmunuhe@gmail.com>
2023-12-19 17:48:46 +0300

    printing numbers of base 10 in a single line

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function to print number of base 10
+ * followed bu a new line
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
