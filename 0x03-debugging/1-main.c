a2082e45385395f36597e6853c8c7f06b1dd4d05
karabanick <nikmunuhe@gmail.com>
2023-12-31 19:20:43 +0300

    commenting an infinite loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes an ifinite loop when i is not increamented(i++)
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
