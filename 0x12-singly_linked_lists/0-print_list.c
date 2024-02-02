824b64175b970940a6ad77c4490f82018f22404f
karabanick <nikmunuhe@gmail.com>
2024-02-02 22:14:42 +0300

    quick fix to displaying list elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,26 +10,35 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	size_t len;
 
 	while (h != NULL)
 	{
-		len = 0;
 
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
-			while (h->str[len] != '\0')
-				len++;
-			printf("[%lu] %s\n", len, h->str);
-		}
+			printf("[%d] %s\n", (int)_strlen(h->str), h->str);
 		count++;
 		h = h->next;
 	}
 
-	printf("-> %lu elements\n", count);
 	return (count);
+}
+
+/**
+ * _strlen - function to calculate length of a string
+ * @str: input string
+ * Return: length
+ */
+
+size_t _strlen(const char *str)
+{
+	size_t len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
 }
