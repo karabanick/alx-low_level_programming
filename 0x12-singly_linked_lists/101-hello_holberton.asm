48849de7a5d834d207066a31dfe1f6fd08c15209
karabanick <nikmunuhe@gmail.com>
2024-02-03 02:23:21 +0300

    assembly code and its compilation

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,7 @@
 section .data
 	hello_msg db "Hello, Holberton", 0
 	format db "%s", 0
+	newline db 10, 0 ; newline character
 
 section .text
 	extern printf
@@ -12,7 +13,12 @@ main:
 	mov rdi, format       ; First argument: pointer to format string
 	mov rsi, hello_msg    ; Second argument: pointer to hello_msg
 	xor rax, rax          ; Clear RAX register for printf to indicate no XMM registers are used for passing
-	call printf           ; Call printf
+	call printf           ; Call printf to print "Hello, Holberton"
+	; Print newline character
+	mov rdi, format       ; First argument: pointer to format string
+	mov rsi, newline      ; Second argument: pointer to newline character
+	xor rax, rax          ; Clear RAX register for printf
+	call printf           ; Call printf to print newline
 	; Clean up the stack
 	pop rbp
 	; Exit the program
